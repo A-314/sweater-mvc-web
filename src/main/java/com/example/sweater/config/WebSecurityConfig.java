7151f4710190f640a6549d3c36a374e951269c1a
Anastasia Emelyan <Nastusha0895@mail.ru>
2022-02-16 12:54:27 +0300

    lesson 3

---
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.sweater.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    protected  void configure(HttpSecurity http)throws Exception{
+        http
+                .authorizeRequests()//включаем авторизацию
+                    .antMatchers("/","registration").permitAll()//для указанного пути"/" включаем полный доступ
+                    .anyRequest().authenticated()//для всех остальных требуем авторизацию
+                .and()
+                    .formLogin()//указываем форму
+                    .loginPage("/login")//указываем мэппинг
+                    .permitAll()//указываем для этого полный доступ
+                .and()
+                    .logout()//также указываем и разрешаем пользоваться всем
+                    .permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username, password, active from usr where username=?")
+                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+    }
+}
